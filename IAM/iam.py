3b9b84de72c0c1db20cc59fce0402c35caa09795
Shubham <pioneer9101993@gmail.com>
2022-07-09 22:53:03 +0530

    Tech[Shubham]

--- IAM/iam.py
+++ IAM/iam.py
@@ -3,4 +3,6 @@ iam=boto3.client('iam')
 paginator=iam.get_paginator('list_users')
 for i in paginator.paginate():
     for j in i['Users']:
-        print(j['UserName']
+        print(j['UserName'])
+
+#-------push token is : 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
