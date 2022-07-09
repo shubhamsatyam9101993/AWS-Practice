a3f0258fd71d2b487f9d5e762df9994cef8b54db
Shubham <pioneer9101993@gmail.com>
2022-07-09 22:47:38 +0530

    Tech[SHubham]

--- IAM/iam.py
+++ IAM/iam.py
@@ -3,4 +3,4 @@ iam=boto3.client('iam')
 paginator=iam.get_paginator('list_users')
 for i in paginator.paginate():
     for j in i['Users']:
-        print(j['UserName'])
+        print(j['UserName']
