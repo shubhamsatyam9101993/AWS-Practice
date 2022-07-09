787c48f2c5ae198b92ee098f9fb869157679e3e6
Shubham <pioneer9101993@gmail.com>
2022-07-10 04:57:03 +0530

    Tech[Shubham]Iam revamped

--- IAM/iam.py
+++ IAM/iam.py
@@ -1,8 +1,16 @@
 import boto3
-iam=boto3.client('iam')
-paginator=iam.get_paginator('list_users')
-for i in paginator.paginate():
-    for j in i['Users']:
-        print(j['UserName'])
 
-#-------push token is : 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
+
+def f1(iam):
+    l1 = []
+    paginator = iam.get_paginator('list_users')
+    for i in paginator.paginate():
+        for j in i['Users']:
+            l1.append(j['UserName'])
+            print(l1)
+
+
+if __name__ == '__main__':
+    iam = boto3.client('iam')
+    f1(iam)
+# -------push token is : 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
