5b2c8a59e5e12b8dfdd7fecf4e49a277608e9ed4
Shubham <pioneer9101993@gmail.com>
2022-07-10 05:49:24 +0530

    Tech[SHubham] iam create user

--- IAM/iam.py
+++ IAM/iam.py
@@ -10,7 +10,14 @@ def f1(iam):
             print(l1)
 
 
+def create_user(iam):
+    response = iam.create_user(UserName='another_user')
+    return response
+
+
 if __name__ == '__main__':
     iam = boto3.client('iam')
     f1(iam)
+    response = create_user(iam)
+    print(response)
 # -------push token is : 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
